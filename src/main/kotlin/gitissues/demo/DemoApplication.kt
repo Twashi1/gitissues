c319690b09cd84ece43f96cb6e15015a33735b7e
Twashi1 <thomas.zarri@gmail.com>
2026-06-13 21:15:27 +0100

    test: HtmlController test

--- src/main/kotlin/gitissues/demo/DemoApplication.kt
+++ src/main/kotlin/gitissues/demo/DemoApplication.kt
@@ -1,11 +1,23 @@
 package gitissues.demo
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication {
+
+  @Bean
+  fun commandLineRunner(ctx: ApplicationContext) = CommandLineRunner {
+    println("Let's inspect the beans provided by Spring Boot:")
+    val beanNames = ctx.beanDefinitionNames
+    beanNames.sorted().forEach { println(it) }
+  }
+}
 
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+  runApplication<DemoApplication>(*args)
 }
+
